149130e1b94e2b052569f2e54dbe46c5521808fa
Volodymyr Parunakian <vladimir.parunakyan@postindustria.com>
2020-03-03 17:53:20 +0200

    Initial Commit

---
+++ Percolation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Percolation
+//
+//  Created by Volodymyr Parunakian on 03.03.2020.
+//  Copyright © 2020 v.parunakian.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
